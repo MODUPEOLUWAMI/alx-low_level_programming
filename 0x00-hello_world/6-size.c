8340db2c4e751065feb56d92431677f0573539b2
MODUPEOLUWAMI <modutoria@gmail.com>
2023-02-17 20:59:53 -0800

    V

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	char d;
 	float f;
 
-printf("size of a int: %1u byte(S)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %1u byte(S)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %1u byte(S)\n", (unsigned long)sizeof(c));
-printf("size of a char: %1u byte(S)\n", (unsigned long)sizeof(d));
-printf("size of a float: %1u byte(S)\n", (unsigned long)sizeof(f));
+printf("size of a int: %1u byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a char: %1u byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
