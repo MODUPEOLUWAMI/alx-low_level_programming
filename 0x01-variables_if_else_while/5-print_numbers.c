b7868582aca472bc3eaeaa580b8b85ff7e980040
MODUPEOLUWAMI <modutoria@gmail.com>
2023-02-20 12:23:06 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <time.h>
+
 
 /**
  * main - Entry point
@@ -16,7 +19,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 		printf("%d", i);
-	printf('\n');
+	printf("\n");
 
 	return (0);
 
