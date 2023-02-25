26f7e8d936114cf662bcf4769f505ba14b9321b9
MODUPEOLUWAMI <modutoria@gmail.com>
2023-02-24 16:31:18 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+
+/**
+ * print_numbers - Print numbers since 0 up to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+
+{
+	char c;
+
+         for (c = '0'; c <= '9'; c++);
+	 {
+		 _putchar(c);
+	 }
+	_putchar('\n');
+}
+
