60be299f512a80b23e6723ced0e0ce78b4b9e0b1
MODUPEOLUWAMI <modutoria@gmail.com>
2023-03-03 03:13:13 -0800

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#index "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: The string to be capitalized
+ * Return: the pointer to the changed string
+ */
+
+char *cap_string(char *str)
+{
+	int index = 0;
