2bb879f1000e58e456b2ce70f90411fcce7b451c
MODUPEOLUWAMI <modutoria@gmail.com>
2023-03-10 20:59:14 -0800

    Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  prints the minimum number of coins to make change for an amount of money
+ * @agrc: argument count
+ * @agrv: argument vector
+ *
+ * Return: -1 or 0
+ */
+
+int main(int agrc, char *agrv[])
+{
+	int cents , coins = 0;
+
+	if (agrc != 2)
+	{
+		printf("Error\n");
+		return(1);
+	}
+
+	cents = atoi(agrv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+                {
+                        cents -= 10;
+                        continue;
+                }
+		if ((cents - 5) >= 0)
+                {
+                        cents -= 5;
+                        continue;
+                }
+		if ((cents - 2) >= 0)
+                {
+                        cents -= 2;
+                        continue;
+                }
+		cents--;
+	}
+	printf("%d\n", coins);
+
+	return (0);
+}
