7967ec522c2af1437046c72dd8e863c0a9b2187f
MODUPEOLUWAMI <modutoria@gmail.com>
2023-03-23 15:41:36 -0700

    a program that performs basic operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,49 +9,53 @@
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
+
 /**
- * op_sub - calculates the difference of two integers
+ * op_sub - calc the difference of two integers
  * @a: the first integer
  * @b: the second integer
  *
- * Return: the difference of a and b
+ * Return: differnce btw a and b
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
+
 /**
- * op_mul - calculates the product of two integers
- * @a: the first integer
- * @b: the second integer
+ * op_mul - calc the product of the integers
+ * @a: the first
+ * @b: the second
  *
- * Return: the product of a and b
+ * Return: product of a and b
  */
 int op_mul(int a, int b)
 {
-        return (a * b);
+	return (a * b);
 }
+
 /**
- * op_div - calculates the division of two integers
- * @a: the first integer
- * @b: the second integer
+ * op_div - calc the division btw a and b
+ * @a: the first
+ * @b: the second
  *
- * Return: the division of a and b
+ * Return: the division btw a and b
  */
 int op_div(int a, int b)
 {
-        return (a / b);
+	return (a / b);
 }
+
 /**
- * op_div - calculates the remainder of the division of two integers
- * @a: the first integer
- * @b: the second integer
+ * op_mod - calc the remainder btw the division of a and b
+ * @a: the first
+ * @b: the second
  *
- * Return: the remainder of a divided by b
+ * Return: the remainder
  */
 int op_mod(int a, int b)
 {
-        return (a % b);
+	return (a % b);
 }
